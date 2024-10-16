ccb3352efbd3a3f2177c122d0df2c6fbb05ec0c1
andrewdonnelly <andrewdonnelly@sytner.co.uk>
2024-10-16 17:28:50 +0100

    Added icons

---
+++ SwiftlyJapaneseTests/SwiftlyJapaneseTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftlyJapaneseTests.swift
+//  SwiftlyJapaneseTests
+//
+//  Created by Andrew Donnelly on 08/10/2024.
+//
+
+import Testing
+@testable import SwiftlyJapanese
+
+struct SwiftlyJapaneseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
