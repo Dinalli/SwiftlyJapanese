1df241adc5507bdbd5084af5ff0c57387465e887
andrewdonnelly <andrewdonnelly@sytner.co.uk>
2024-10-17 17:51:24 +0100

    Added screens

---
+++ SwiftlyJapanese/Dashboard/Dashboard.swift
@@ -0,0 +1,18 @@
+//
+//  Dashboard.swift
+//  SwiftlyJapanese
+//
+//  Created by Andrew Donnelly on 16/10/2024.
+//
+
+import SwiftUI
+
+struct Dashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Dashboard()
+}
