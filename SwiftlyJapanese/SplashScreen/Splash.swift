3f75c483777026aa112c204fd0d71bf21a73617e
andrewdonnelly <andrewdonnelly@sytner.co.uk>
2024-10-18 19:26:23 +0100

    Animations done

--- SwiftlyJapanese/SplashScreen/Splash.swift
+++ SwiftlyJapanese/SplashScreen/Splash.swift
@@ -11,36 +11,55 @@ struct Splash: View {
     @State var sunOffset = 40.0
     @State var leftCloudOffset = -40.0
     @State var rightCloudOffset = 40.0
+    @State var titleText = "Swiftly Japanese"
+    @State var shadowColor: Color = .green
 
     var body: some View {
-        ZStack {
-            Color("SplashBackground")
-            Image("smallcloud").resizable()
-                .frame(width: 144,height: 144)
-                .offset(y:sunOffset)
-            Image("sunlight").resizable()
-                .frame(width: 144,height: 144)
-                .offset(y:sunOffset)
-            Image("Fuji").resizable()
-                .frame(width: 144,height: 144)
-            Image("leftcloud").resizable()
-                .frame(width: 144,height: 144)
-                .offset(x:leftCloudOffset)
-            Image("rightcloud").resizable()
-                .frame(width: 144,height: 144)
-                .offset(x:rightCloudOffset)
-        }.ignoresSafeArea(.all)
-            .onAppear {
-                withAnimation(.easeInOut(duration: 3)) {
-                    sunOffset = 0
+        GeometryReader { geo in
+            ZStack {
+                Color("SplashBackground")
+                Image("smallcloud").resizable()
+                    .frame(width: 144,height: 144)
+                    .offset(y:sunOffset)
+                Image("sunlight").resizable()
+                    .frame(width: 144,height: 144)
+                    .offset(y:sunOffset)
+                Image("Fuji").resizable()
+                    .frame(width: 144,height: 144)
+                Image("leftcloud").resizable()
+                    .frame(width: 144,height: 144)
+                    .offset(x:leftCloudOffset)
+                Image("rightcloud").resizable()
+                    .frame(width: 144,height: 144)
+                    .offset(x:rightCloudOffset)
+                VStack {
+                    Text(titleText)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.white)
+                        .font(.system(size: 50, weight: .heavy))
+                        .frame(width: 250)
+                        .shadow(color: shadowColor, radius: 5)
+                        .shadow(color: shadowColor, radius: 5 )
+                        .position(x: geo.size.width/2, y: geo.size.height/3)
                 }
-                withAnimation(.easeInOut(duration: 3)) {
-                    leftCloudOffset = 0
+            }.ignoresSafeArea(.all)
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 3)) {
+                        sunOffset = 0
+                    }
+                    withAnimation(.easeInOut(duration: 3)) {
+                        leftCloudOffset = 0
+                    }
+                    withAnimation(.easeInOut(duration: 3)) {
+                        rightCloudOffset = 0
+                    }
+                    withAnimation(.linear(duration: 3).repeatForever(autoreverses: true)) {
+                        titleText = "素早く日本語化"
+                        shadowColor = .red
+                    }
+
                 }
-                withAnimation(.easeInOut(duration: 3)) {
-                    rightCloudOffset = 0
-                }
-            }
+       }
     }
 }
 
--- SwiftlyJapanese/SwiftlyJapaneseApp.swift
+++ SwiftlyJapanese/SwiftlyJapaneseApp.swift
@@ -25,7 +25,7 @@ struct SwiftlyJapaneseApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Splash()
         }
         .modelContainer(sharedModelContainer)
     }
