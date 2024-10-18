642bad26695ab1ddc765688f67ef87d864401893
andrewdonnelly <andrewdonnelly@sytner.co.uk>
2024-10-18 17:53:21 +0100

    animations

--- SwiftlyJapanese/SplashScreen/Splash.swift
+++ SwiftlyJapanese/SplashScreen/Splash.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct Splash: View {
+    @State var sunOffset = 40.0
+    @State var leftCloudOffset = -40.0
+    @State var rightCloudOffset = 40.0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("SplashBackground")
+            Image("smallcloud").resizable()
+                .frame(width: 144,height: 144)
+                .offset(y:sunOffset)
+            Image("sunlight").resizable()
+                .frame(width: 144,height: 144)
+                .offset(y:sunOffset)
+            Image("Fuji").resizable()
+                .frame(width: 144,height: 144)
+            Image("leftcloud").resizable()
+                .frame(width: 144,height: 144)
+                .offset(x:leftCloudOffset)
+            Image("rightcloud").resizable()
+                .frame(width: 144,height: 144)
+                .offset(x:rightCloudOffset)
+        }.ignoresSafeArea(.all)
+            .onAppear {
+                withAnimation(.easeInOut(duration: 3)) {
+                    sunOffset = 0
+                }
+                withAnimation(.easeInOut(duration: 3)) {
+                    leftCloudOffset = 0
+                }
+                withAnimation(.easeInOut(duration: 3)) {
+                    rightCloudOffset = 0
+                }
+            }
     }
 }
 
