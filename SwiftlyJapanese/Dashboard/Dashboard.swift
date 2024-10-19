a893a5e406cee34a6ed51d80537e8148a84e4b4a
andrewdonnelly <andrewdonnelly@sytner.co.uk>
2024-10-19 16:23:51 +0100

    Added info launch screen

--- SwiftlyJapanese/Dashboard/Dashboard.swift
+++ SwiftlyJapanese/Dashboard/Dashboard.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct Dashboard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("SplashBackground")
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
